dca2529dc0739e4e7ca6f7cfe31a3404a40763dc
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-09-13 22:21:47 +0800

    Added day 1 of week 2 - node

---
+++ week-2/day1.js
@@ -0,0 +1,17 @@
+console.log('Hello Js!');
+
+const os = require('os');
+
+console.log("Platform: ", os.platform());
+console.log("CPU: ", os.cpus().length);
+console.log("Free Memory: ", os.freemem());
+
+// Do the following:
+// Print "Node.js is awesome!".
+// Use os module to print your platform, total memory, and free memory.
+// Add a script in package.json called "dev" that runs day1.js.
+
+console.log("Node.JS is awesome!");;
+console.log("Platform: ", os.platform());
+console.log("Total Memory: ", os.totalmem());
+console.log("Free Memory: ", os.freemem());
