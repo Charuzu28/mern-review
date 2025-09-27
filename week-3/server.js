452125d039925feefd384a130bfe9f3600918803
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-09-27 23:34:25 +0800

    Added week 3  last day  Production ready ExressJS

--- week-3/server.js
+++ week-3/server.js
@@ -1,19 +1,43 @@
+// const express = require('express');
+// const app = express();
+// const PORT = 5000;
+
+// const notesRoutes = require('./routes/notes')
+
+// app.use(express.json());
+
+// app.use('/notes', notesRoutes);
+
+// app.use((err, req,res,next) => {
+//     console.error(err.stack);
+//     res.status(500).json({success: false, message: "Server Error!"})
+// })
+
+// app.listen(PORT,() => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// });
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const notesRoutes = require('./routes/notes');
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-const notesRoutes = require('./routes/notes')
-
+app.use(cors());
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use('/notes', notesRoutes);
 
-app.use((err, req,res,next) => {
+app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({success: false, message: "Server Error!"})
-})
-
-app.listen(PORT,() => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    res.status(500).json({success: false, message: "Server Error!"});
 });
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+
+})
 
