10d8f4f723fd23192c798c8ebb50f63a1ea10e23
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-12-02 22:50:03 +0800

    Added day 1 in week-1 for refresher

--- week-1/Day1.js
+++ week-1/Day1.js
@@ -1,11 +1,67 @@
-let x = 'Hello';
-let y = 1;
-let z = [1,2,3,4];
-let a = true;
+const user = "Charles";
+let score = 10;
+
+console.log(typeof(user));
+console.log(typeof(score));
+
+score = score + 10;
+console.log(`The new score is ${score}`);
+
+// Assignment A
+    // Create variables for:
+    // Your name
+    // Your age
+    // Your favorite programming language
+    // Whether you like cats
+    // Your birthday (string)
+    // Then log them using template literals.
+        
+    const name = "Charles";
+    const age = 24;
+    const progLanguage = "Javascript";
+    const likeCats = true;
+    const birthday = "08/28/2001";
+
+    console.log(`Hi, my name is ${name}. I'm ${age} y/o, my favorite programming language is ${progLanguage}. Do I like cats? ${likeCats}. I was born in ${birthday}.`)
+
+//Assignment B
+    // Show me:
+    // strict equality (===)
+    // loose equality (==)
+    // explain what happened
+const num1 = 5;
+const num2 = '5';
+
+if(num1 == num2){
+    console.log("loose equality") // it shows that the string 5 is converted to 5 so its means even they're not actually
+                                    // equal in == it considered because the string 5 is converted to number - note in == it converted types
+}
+if(num1 === num2){              // Here as you can see it doesnt convert the type because === is more strict means it must
+                                //  be   identical or really equal to be true
+    console.log("not equal") 
+}else{
+    console.log("strict equality")
+}
+
+// Assignment C
+    // Predict the output (NO RUNNING FIRST):
+    // console.log("5" + 1); //Answer: 7
+    // console.log("5" - 1); //Answer:  4
+    // console.log("5" * "2"); //Answer:  10
+    // console.log("hello" * 2); //Answer:  hello 2
+    // Give explanations.
+    // Ii think its because of auto conversion of some types that some string can be calculated by string to number
 
 
-console.log(typeof x);
-console.log(typeof y);
-console.log(typeof z);
-console.log(typeof a);
-// DATA TYPES
+// DAY 1 — INTERVIEW QUIZ
+// Answer these WITHOUT guessing:
+// Difference between let and const?
+    // The difference between let and const are let can be changed while constant is the variable will be cosntant means cannot be replaced or changed;
+// Why is var dangerous?
+    //var is dangerous becuase its a legacy and old means it has bugs.
+// What does === do that == does not?
+    //The === do is its more strict and always check even its same data but different types and it doesnt convert types. While the == is not that strict so it converts some types and just they are equal data they can be true.
+// What is dynamic typing?
+    // Dynamic typing is the constant change of variable.
+// What is a template literal?
+    // Template literal is enclosed with backticks note quoation marks
