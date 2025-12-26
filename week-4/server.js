adcb6faf46bb5d71eea7381811dbe825c0a224a6
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-12-26 22:53:32 +0800

    Added CRUD - API

--- week-4/server.js
+++ week-4/server.js
@@ -2,6 +2,7 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import express from 'express';
 import User from './model/User.js';
+import userRoutes from './routes/userRoutes.js'
 
 const PORT = 5000;
 dotenv.config();
@@ -13,16 +14,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("API is running...");
 })
-
-const testUser = async () => {
-    const user = await User.create({
-        name: "Charles",
-        age: 24
-    });
-    console.log(user);
-}
-
-testUser();
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
