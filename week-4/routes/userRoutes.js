6df898ff4219c604f500f12978f26d1f785894d6
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-12-30 21:09:00 +0800

    Added day 3 advance queries, validation, and indexing

--- week-4/routes/userRoutes.js
+++ week-4/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import User from '../model/User.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
@@ -13,65 +14,105 @@ router.post('/', async (req, res) => {
         }
 
         const user = await User.create({name, age});
-        res.status(200).json(user);
+        res.status(201).json(user);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 });
-// GET ALL USER
-router.get('/', async (req, res) => {
+// GET ACTIVE USER
+router.get('/active', async (req, res) => {
     try {
-        const user = await User.find();
-        res.status(200).json(user);
+        const page = Number(req.query.page) || 1;
+        const limit = 5;
+        const skip = (page - 1) * limit;
+
+        const user = await User.find(
+        {active: true}, {name: 1, age: 1}
+        )
+        .skip(skip)
+        .limit(limit)
+
+        if(user.length === 0){
+            return res.status(404).json({message: "User not found!"})
+        }
+
+        res.status(200).json(user)
     } catch (error) {
         res.status(500).json({message: error.message})
-    }
+    }    
 })
-// GET ONE USER
+// GET ONE USER { DEFENSIVE }
 router.get('/:id', async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({message: "Invalid ID!"});
+        }
+
+        const user = await User.findById(id);
 
         if(!user){
             return res.status(404).json({message: "User not found!"})
         }
-        res.status(200).json(user);
+        res.json(user);
     } catch (error) {
-       res.status(400).json({message: "Invalid ID"}) 
+        res.status(500).json({message: error.message});
     }
+    
 })
+
 
 // UPDATE
 router.put('/:id', async (req, res) => {
-try {
-    const user = await User.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new: true, runValidators: true}
-    )
-    if(!user){
-            return res.status(404).json({message: "User not found!"})
-    }
+    try {
+        const { id } = req.params;
 
-    res.status(200).json(user)
-} catch (error) {
-    res.status(400).json({message: "Invalid ID"});
-}
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({message: "Invalid ID!"})
+        }
+
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true, runValidators: true}
+        )
+        
+        if(!user){
+                return res.status(404).json({message: "User not found!"})
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+    
+ 
 });
 
 // DELETE
-router.delete('/:id', async (req, res) => {
+router.delete('/:id/deactivate', async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({message: "Invalid ID!"});
+        }
+
+        const user = await User.findByIdAndUpdate(
+            id,
+            {active: false},
+            {new: true}
+        );
 
         if(!user){
             return res.status(404).json({message: "User not found!"})
         }
 
-        res.status(200).json({message: "User deleted!"});
+        res.status(200).json({message: "User deactivated!"});
     } catch (error) {
-        res.status(400).json({message: "Invalid ID"});
+        res.status(500).json({message: error.message});
     }
+    
+    
 })
 
 export default router;
